52d8f436ff0c34c00e1600888ef0bd86c845f2ba
Takanori Arita <arita-t@nexceed.co.jp>
2016-11-13 20:35:17 +0900

    add Traders

---
+++ app/views/traders/_trader.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! trader, :id, :name, :created_at, :updated_at
+json.url trader_url(trader, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113082154) do
+ActiveRecord::Schema.define(version: 20161113113043) do
 
   create_table "tests", force: :cascade do |t|
     t.text     "memo",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "traders", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "users", force: :cascade do |t|
