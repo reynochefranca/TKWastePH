d643af4bc6f3dc58d16aa3458fe548dce1b00f3a
Masuda Yuta <masuda.y@kanamekey.com>
2017-01-30 01:22:42 +0000

    Delete duplicate permission migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170122015952) do
+ActiveRecord::Schema.define(version: 20170125035200) do
 
   create_table "cars", force: :cascade do |t|
     t.string   "car_type",   limit: 255
